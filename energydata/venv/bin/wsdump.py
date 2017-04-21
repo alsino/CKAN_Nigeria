afa713fc7652c195ec707c7da3940ab71631f1ee
alsino <iamahorse@gmail.com>
2017-04-21 15:01:58 +0200

    commit

---
+++ energydata/venv/bin/wsdump.py
@@ -0,0 +1,4 @@
+#!/home/sysadmin/energydata/venv/bin/python
+# EASY-INSTALL-SCRIPT: 'websocket-client==0.40.0','wsdump.py'
+__requires__ = 'websocket-client==0.40.0'
+__import__('pkg_resources').run_script('websocket-client==0.40.0', 'wsdump.py')
